7453680e0d49323708133dc6fd88f3ac41d76335
mbcladwell <mbcladwell@stihie.net>
2020-04-01 17:20:52 -0400

    first with make

---
+++ environment.scm
@@ -0,0 +1,2 @@
+(specifications->manifest
+		'("emacs" "guile" "guile-dbi" "guile-dbd-postgresql" "artanis" "coreutils" "sed" "gawk" "findutils" "glibc" "openssl" "postgresql" "grep" "r" "r-ggplot2"))
---
+++ tpl/content.tpl
@@ -0,0 +1,4 @@
+<a href="some.link">My Link</a>
+
+<a href=" <%= help-link %> "> <%= help-text %> </a>
+
