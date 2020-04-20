20966fe49bf8d4084ebc258bb6b7d39098e9f69e
mbcladwell <mbcladwell@stihie.net>
2020-04-20 11:37:16 -0400

    mon morn

---
+++ manifest.scm
@@ -0,0 +1,3 @@
+(specifications->manifest
+		'("guile@2.2.7" "guile-dbi" "guile-dbd-postgresql" "artanis@0.4.1" "coreutils" "gawk" "sed" "findutils" "glibc" "grep"  "openssl" "postgresql"  "r" "rest" ))
+
